aa26fa820155dfbdce32cf0752a8067caa09e012
yueyong <yueyong1030@outlook.com>
2024-09-17 18:31:29 +0800

    add new frames, add alert to choose different frames

---
+++ OneMonitor/View/SectionView.swift
@@ -0,0 +1,18 @@
+//
+//  SectionView.swift
+//  OneMonitor
+//
+//  Created by EnochLiang on 2024/9/17.
+//
+
+import SwiftUI
+
+struct SectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionView()
+}
