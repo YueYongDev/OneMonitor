c24778a08293f137f6cde1302b190f28d680bedd
yueyong <yueyong1030@outlook.com>
2024-10-10 02:12:31 +0800

    更改了todolist

---
+++ OneMonitor/View/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  OneMonitor
+//
+//  Created by EnochLiang on 2024/10/10.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoListView()
+}
---
+++ OneMonitor/ViewModel/TodoListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TodoListViewModel.swift
+//  OneMonitor
+//
+//  Created by EnochLiang on 2024/10/10.
+//
+
+import Foundation
